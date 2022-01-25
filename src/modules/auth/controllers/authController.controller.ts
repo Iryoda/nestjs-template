c8df0f57735669fecf63a1da76772bdcb1fb8c89
Augusto Iryoda <iryodap@gmail.com>
2022-01-25 10:39:05 -0300

    :sparkles: feat: add user swagger documentation

--- src/modules/auth/controllers/authController.controller.ts
+++ src/modules/auth/controllers/authController.controller.ts
@@ -1,9 +1,12 @@
-
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Public } from 'src/shared/decorators/public.decorator';
 import { LocalAuthGuard } from 'src/shared/guards/auth/local-auth.guard';
+import { LogInResponseDTO } from '../dtos/logInResponseDTO';
+import { LogInUserDTO } from '../dtos/logInUserDTO';
 import { AuthUseCase } from '../useCases/authUseCase.service';
 
+@ApiTags('Session')
 @Controller()
 export class AuthController {
   constructor(private authUseCase: AuthUseCase) {}
@@ -11,6 +14,8 @@ export class AuthController {
   @Public()
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
+  @ApiBody({ type: LogInUserDTO })
+  @ApiResponse({ status: 200, type: LogInResponseDTO })
   async login(@Request() req: any) {
     return await this.authUseCase.login(req.user);
   }
--- src/modules/user/controllers/findUser.controller.ts
+++ src/modules/user/controllers/findUser.controller.ts
@@ -6,14 +6,17 @@ import {
   ParseUUIDPipe,
   UseInterceptors,
 } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { User } from '../domain/User';
 import { FindUserUseCase } from '../useCases/findUserUseCase.service';
 
+@ApiTags('User')
 @Controller()
 export class FindUserController {
   constructor(private findUserUseCase: FindUserUseCase) {}
 
   @UseInterceptors(ClassSerializerInterceptor)
+  @ApiResponse({ type: User })
   @Get('users/:id')
   async byId(@Param('id', ParseUUIDPipe) id: string) {
     const user = await this.findUserUseCase.byId(id);

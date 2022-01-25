3b71d5e34d9f5a04f01ac6e69ba5cd6fa1cf0d7f
Augusto Iryoda <iryodap@gmail.com>
2022-01-25 10:36:36 -0300

    :wrench: add: swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
-import { ValidationPipe } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+
+const createSwaggerDocument = (app: INestApplication) => {
+  const config = new DocumentBuilder()
+    .setTitle('Nest Template')
+    .setDescription('An template to initiate projects made by Augusto Iryoda')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
   app.useGlobalPipes(new ValidationPipe());
+
+  createSwaggerDocument(app);
 
   await app.listen(process.env.PORT || 3333);
 }

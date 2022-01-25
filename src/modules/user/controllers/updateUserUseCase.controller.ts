c46e30ed7b61514c684c48856349c642ed7de8b5
Augusto Iryoda <iryodap@gmail.com>
2022-01-25 10:39:05 -0300

    :sparkles: feat: add user swagger documentation

--- src/modules/user/controllers/updateUserUseCase.controller.ts
+++ src/modules/user/controllers/updateUserUseCase.controller.ts
@@ -9,12 +9,15 @@ import {
 import { User } from '../domain/User';
 import { UpdateUserDTO } from '../dtos/updateUserDTO';
 import { UpdateUserUseCase } from '../useCases/updateUserUseCase.service';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('User')
 @Controller()
 export class UpdateUserController {
   constructor(private updateUserUseCase: UpdateUserUseCase) {}
 
   @UseInterceptors(ClassSerializerInterceptor)
+  @ApiResponse({ type: User })
   @Patch('users/:id')
   async update(@Param('id') id: string, @Body() data: UpdateUserDTO) {
     const user = await this.updateUserUseCase.handle(id, data);

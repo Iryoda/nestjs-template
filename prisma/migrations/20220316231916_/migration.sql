06a57d3ddc417930d29abc7669a1bcf6568669f2
Augusto Iryoda <iryodap@gmail.com>
2022-03-16 20:22:02 -0300

    :fire: remove: old migrations

--- prisma/migrations/20220316231916_/migration.sql
+++ prisma/migrations/20220316231916_/migration.sql
@@ -4,6 +4,8 @@ CREATE TABLE "User" (
     "username" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
+    "createAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updateAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
